34291171c6cf051d252860231b915131da569b03
amadeus127 <nitram.reyess@hotmail.com>
2024-05-15 16:31:47 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -1,11 +1,13 @@
+alert("1");
 let respuesta = document.getElementById("respuesta");
 
 function callApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   // .get("https://18.132.200.102/iot-car-control/back-end/apis/getRegistro.php")
   axios
+    alert("2");
     .get("https://18.169.105.156/iot-car-control/back-end/apis/getRegistro.php")
-    
+    alert("3);
     .then(function (response) {
       // manejar respuesta exitosa
       console.log(response);
